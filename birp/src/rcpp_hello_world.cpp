fccd2cd90cb6a19acfbe27d11d07fe0027be563b
madleina <madleina.caduff@gmx.ch>
2024-04-30 10:13:47 +0200

    use main.run()

--- birp/src/rcpp_hello_world.cpp
+++ birp/src/rcpp_hello_world.cpp
@@ -21,9 +21,7 @@ List birp_interface(List input) {
   main.addRegularTask("infer", new TTask_infer());
   
   // now run program
-  // return main.run(input);
-  
-  return input;
+  return main.run(input.size(), input);
 }
 
 // [[Rcpp::export]]
