5e46e6923e31900affa8d1852a0c682b87591fc3
madleina <madleina.caduff@gmx.ch>
2025-05-08 14:59:43 +0200

    fixed too long line

--- R/birp_data.R
+++ R/birp_data.R
@@ -180,7 +180,8 @@ birp_data <- function(counts, efforts, times, CI_groups = NULL, location_names =
 #' @param sep The field separator character
 #' @return An object of type \link{birp_data}
 #' @examples
-#' filenames <- file.path(system.file("extdata", package = "birp"), "birp_Method_1_simulated_counts.txt")
+#' dir <- system.file("extdata", package = "birp")
+#' filenames <- file.path(dir, "birp_Method_1_simulated_counts.txt")
 #' data <- birp_data_from_file(filenames = filenames, sep = "\t")
 #' @export
 birp_data_from_file <- function(filenames, method_names = NA, sep = ","){
